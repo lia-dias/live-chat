b2cdbfc1d08a55f57322ea20f35a32a843f6dd2f
Lia Dias <liadias914@gmail.com>
2022-12-15 15:44:24 -0300

    Initial Commit

---
+++ live-chat/live_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  live_chatApp.swift
+//  live-chat
+//
+//  Created by Lia Dias on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct live_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
